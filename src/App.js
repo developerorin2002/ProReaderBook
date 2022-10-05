86f5078196407d58e008c6cbd1e6764c1a24472d
developerorin2002 <developerorin2002@gmail.com>
2022-10-06 00:48:28 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React from 'react'
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home'
+import LayoutMain from './LayoutMain';
+import About from './components/About'
+import Books from './components/Books'
+import BookDetails from './components/BookDetails';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<LayoutMain></LayoutMain>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'/home',
+        element:<Home></Home>
+      },
+      {
+        path:'/about',
+        element:<About></About>
+      },
+      {
+        path:'/books',
+        element:<Books></Books>,
+        loader:()=> fetch('https://api.itbook.store/1.0/new')
+      },
+      {
+        path:'/book/:id',
+        element:<BookDetails></BookDetails>,
+        loader:({params})=>fetch(`https://api.itbook.store/1.0/books/${params.id}`)
+      }
+    ]
+  }
+])
 function App() {
-  return <h1> Welcome to proReader</h1>
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+
 }
 
 export default App
